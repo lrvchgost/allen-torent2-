b30c315e8773c22e16ce8f0d443e29d6ba97883d
Roman Lonskii <lrvchgost@gmail.com>
2026-08-25 23:10:23 +0300

    bug fix

--- src/download/bitfield-handler.js
+++ src/download/bitfield-handler.js
@@ -3,7 +3,7 @@
 const requestPiece = require('./request-piece');
 
 module.exports = function bitfieldHandler(socket, pieces, queue, payload) {
-  const queueEmpty = queue.length === 0;
+  const queueEmpty = queue.length() === 0;
   payload.forEach((byte, i) => {
     for (let j = 0; j < 8; j++) {
       if (byte % 2) queue.queue(i * 8 + 7 - j);
--- src/download/have-handler.js
+++ src/download/have-handler.js
@@ -4,7 +4,7 @@ const requestPiece = require('./request-piece');
 
 module.exports = function haveHandler(socket, pieces, queue, payload) {
   const pieceIndex = payload.readUInt32BE(0);
-  const queueEmpty = queue.length === 0;
+  const queueEmpty = queue.length() === 0;
   queue.queue(pieceIndex);
   if (queueEmpty) requestPiece(socket, pieces, queue);
 };
